1308b53ce71da8c0e45173c1afa62a7c1bcd4f4d
Alexey <aleksma99@yandex.ru>
2018-12-05 13:18:19 +0300

    Traceroute project on tour

---
+++ Go/Traceroute project (hi dudes how can i ddos here)/tracerouteProject.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+
+	"net/http"
+	"github.com/mgutz/logxi/v1"
+	"fmt"
+)
+
+func HomeRouterHandler2(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+	yourUrl := r.Form["yourl"][0]
+
+	fmt.Fprint(w, yourUrl)
+
+}
+
+func HomeRouterHandlerStart(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Println(r.URL.Path[1:5])
+	if r.URL.Path == "/" {
+		http.ServeFile(w, r, "index.html")
+	}
+
+}
+
+
+func main() {
+
+	http.HandleFunc("/", HomeRouterHandlerStart)
+	http.HandleFunc("/process", HomeRouterHandler2)
+
+	err := http.ListenAndServe(":10053", nil)
+
+	if err != nil {
+
+		log.Fatal("ListenAndServe: ", err)
+
+	}
+
+}
