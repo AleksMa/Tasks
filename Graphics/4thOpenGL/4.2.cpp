ae8b64f2b1b25a7bac0a0cb945214a51d75d0c95
Alexey Mamaev <aleksma99@yandex.ru>
2019-04-11 08:47:45 +0300

    Update 4.2.cpp

--- Graphics/4thOpenGL/4.2.cpp
+++ Graphics/4thOpenGL/4.2.cpp
@@ -5,20 +5,20 @@ using namespace std;
 const int N = 3;
 
 struct Point {
-  GLint x;
-  GLint y;
+  int x;
+  int y;
 };
 
-GLfloat *buff;
+float *buff;
 vector<Point> points;
 
-bool filterFlag = false;
+bool fl = false;
 
-int height = 800;
-int width = 800;
+int height = 800,
+    width = 800;
 
 void fillBuf() {
-  fill_n(buff, height * width * N, 1);
+  fill_n(buff, height * width * N, 0.8);
 }
 
 int sign(int x) {
@@ -45,7 +45,7 @@ void drawLine(const Point &pA, const Point &pB) {
 
   if (dy > dx) {
     flag = true;
-    std::swap(dx, dy);
+    swap(dx, dy);
   }
 
   int k = 2 * dy - dx;
@@ -75,8 +75,8 @@ void drawLine(const Point &pA, const Point &pB) {
 }
 
 void filterFrame() {
-  const int kernel[] = {0, 1, 0, 1, 2, 1, 0, 1, 0};
-  const double norm = 6;
+  const int base[] = {0, 1, 0, 1, 2, 1, 0, 1, 0};
+  const double n = 6;
 
   for (int y = 1; y < height - 1; y++) {
     for (int x = 1; x < width - 1; x++) {
@@ -84,42 +84,23 @@ void filterFrame() {
 
       for (int i = x - 1; i <= x + 1; i++) {
         for (int k = y - 1; k <= y + 1; k++) {
-          c += kernel[(i - x + 1) * N - y + 1 + k] * buff[(i + k * width) * N];
+          c += base[(i - x + 1) * N - y + 1 + k] * buff[(i + k * width) * N];
         }
       }
 
       for (int i = 0; i < N; i++) {
-        buff[(x + y * width) * N + i] = c / norm;
+        buff[(x + y * width) * N + i] = c / n;
       }
     }
   }
 }
 
-void draw(GLFWwindow *window) {
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  fillBuf();
-
-  if (!points.empty()) {
-    for (int i = 0; i < points.size(); i++)
-      drawLine(points[i], points[(i + 1) % points.size()]);
-  }
-
-  if (filterFlag) {
-    filterFrame();
-  }
-
-  glDrawPixels(width, height, GL_RGB, GL_FLOAT, (void *) buff);
-  glfwSwapBuffers(window);
-}
-
 void buffer_callback(GLFWwindow *window, int w, int h) {
-  height = h;
-  width = w;
+  glfwGetFramebufferSize(window, &width, &height);
 
   delete[] buff;
   points.clear();
-  filterFlag = false;
+  fl = false;
 
   buff = new GLfloat[width * height * N];
   fillBuf();
@@ -129,12 +110,11 @@ void buffer_callback(GLFWwindow *window, int w, int h) {
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
   if (action == GLFW_PRESS) {
     switch (key) {
-
-      case GLFW_KEY_ENTER:filterFlag = !filterFlag;
+      case GLFW_KEY_ENTER:fl = !fl;
         break;
 
-      case GLFW_KEY_DELETE:points.clear();
-        filterFlag = false;
+      case GLFW_KEY_BACKSPACE:points.clear();
+        fl = false;
         fillBuf();
         break;
 
@@ -181,7 +161,21 @@ int main() {
   glViewport(0, 0, width, height);
 
   while (!glfwWindowShouldClose(window)) {
-    draw(window);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    fillBuf();
+
+    if (!points.empty()) {
+      for (int i = 0; i < points.size(); i++)
+        drawLine(points[i], points[(i + 1) % points.size()]);
+    }
+
+    if (fl) {
+      filterFrame();
+    }
+    glDrawPixels(width, height, GL_RGB, GL_FLOAT, buff);
+
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
